653103267bf2a6fa5f49b74fd435e402720fcbd3
fernando7492 <emidio8000@gmail.com>
2025-12-13 16:39:44 -0300

    feat/invoice: implement invoice creation and management functionality

---
+++ backend/src/invoices/invoices.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateInvoiceDto } from './dto/create-invoice.dto';
+import { UpdateInvoiceDto } from './dto/update-invoice.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class InvoicesService {
+  constructor(private prisma: PrismaService) {}
+  create(createInvoiceDto: CreateInvoiceDto) {
+    return 'This action adds a new invoice';
+  }
+
+  findAll() {
+    return `This action returns all invoices`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} invoice`;
+  }
+
+  update(id: string, updateInvoiceDto: UpdateInvoiceDto) {
+    return `This action updates a #${id} invoice`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} invoice`;
+  }
+}
