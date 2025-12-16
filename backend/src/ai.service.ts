341903a6e311183df0d1993f2f91286975b18b3b
fernando7492 <emidio8000@gmail.com>
2025-12-16 11:39:57 -0300

    config/render: change the gemini version from flash-latest to let google decide the model

--- backend/src/ai.service.ts
+++ backend/src/ai.service.ts
@@ -12,7 +12,7 @@ export class AiService {
     }
     this.genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     this.model = this.genAI.getGenerativeModel({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-flash-latest',
     });
   }
 
